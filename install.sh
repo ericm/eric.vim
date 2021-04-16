f1a757aaebe54e8dd13a2ea32ce1c80a54f3aacd
Eric Moynihan <git@moynihan.io>
2021-04-16 21:00:16 +0100

    MKDIR

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 ln -fns $PWD/.vimrc $HOME/.vimrc
+mkdir -p $HOME/.vim
 ln -fns $PWD/coc-settings.json $HOME/.vim/coc-settings.json
 ln -fns $PWD/.editorconfig $HOME/.editorconfig
